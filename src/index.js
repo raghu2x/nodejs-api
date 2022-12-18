ba6523206b40b59ef69611669cad64b8a50467db
raghvendra <raghvendrayadav40771@gmail.com>
2022-12-18 17:57:41 +0530

    fix depoyment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
 
-const { API_PORT } = process.env;
+const { API_PORT, PORT } = process.env;
 
-app.listen(API_PORT, () => {
+app.listen(PORT || API_PORT, () => {
   console.log(`________________server is running at port ${API_PORT}`);
 });
 
