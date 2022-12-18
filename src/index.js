5b4955da64b704f5402049555e6dfa0c59a84390
raghvendra <raghvendrayadav40771@gmail.com>
2022-12-18 17:41:13 +0530

    fixed deployment issues

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const server = http.createServer(app);
 
 const { API_PORT } = process.env;
 
-server.listen(API_PORT, () => {
+app.listen(API_PORT, () => {
   console.log(`________________server is running at port ${API_PORT}`);
 });
