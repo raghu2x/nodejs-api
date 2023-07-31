ab2c02521a4dc6cb60075b9edc5808354cb24157
raghvendra <raghvendrayadav40771@gmail.com>
2023-08-01 01:18:58 +0530

    code fixes

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -1,12 +1,12 @@
-const router = require('express').Router()
-const authController = require('../controllers/authController')
+const router = require('express').Router();
+const authController = require('../controllers/authController');
 
-router.post('/login', authController.loginAccount)
-router.post('/register', authController.createAccount)
+router.post('/login', authController.loginAccount);
+router.post('/register', authController.createAccount);
 // router.post('/send-otp', authController.sendOTP)
-router.post('/send-email', authController.sendEmail)
-router.post('/verify-contact', authController.verifyAccount)
-router.post('/forgot-password', authController.forgotPassword)
-router.post('/reset-password/:token', authController.resetPassword)
+router.post('/send-email', authController.sendEmail);
+router.post('/verify-contact', authController.verifyAccount);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password/:token', authController.resetPassword);
 
-module.exports = router
+module.exports = router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const auth = require('../middleware/auth')
-const testRoute = require('./test')
+const routes = require('./routes')
 const authRouter = require('./authRouter')
 const crudRouter = require('../crud/apiRouter')
 
 router.use('/api/auth', authRouter)
 router.use('/api', auth, crudRouter)
-router.use('/', testRoute)
+router.use('/', routes)
 
 router.all('*', (req, res) => {
   res.status(404).json({
