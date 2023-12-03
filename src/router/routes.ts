d437dd38f66a789d6bd4e4d923ad4e57cf7c4174
Raghvendra Yadav <raghvendraa.dev@gmail.com>
2023-12-04 00:59:11 +0530

    Up: Removed deprecated dependancies

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,6 @@ import { getDBModel } from 'database/connection'
 import AppError from 'utils/appError'
 import httpStatus from 'http-status'
 import { sendErrorResponse, sendSuccessResponse } from 'utils/apiResponse'
-// import IP from 'ip'
 
 const router: Router = Router()
 
@@ -27,22 +26,11 @@ router.get('/api/env', (req: Request, res: Response) => {
 })
 
 router.get('/api/endpoints', (req: Request, res: Response) => {
-  // const ipAddress = req.headers['x-forwarded-for'] ?? req.connection.remoteAddress
-  // const userAgent = req.get('user-agent')
-  // const ip = IP.address()
-
   res.sendFile('resume.pdf', { root: 'public' }, function (err) {
     console.log(err)
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (err) res.status(500).json({ success: false, message: "couldn't find file" }) // send a 500 on error
   })
-  // res.send({
-  //   success: true,
-  //   ipAddress,
-  //   userAgent,
-  //   ip
-  //   // data: Object.keys(models),
-  // })
 })
 
 // const sendEmail = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
