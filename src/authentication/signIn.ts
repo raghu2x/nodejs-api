ed0270c637dbe248a96e1612cc0f8066c12eacba
Raghvendra Yadav <raghvendraa.dev@gmail.com>
2024-01-08 02:12:13 +0530

    feat: admin signUp flow

--- src/authentication/signIn.ts
+++ src/authentication/signIn.ts
@@ -1,14 +1,18 @@
 import { type AuthenticatedRequest, type LoginData } from '../utils/interfaces'
 import { type Response, type NextFunction } from 'express'
-import { createModel, useDB } from '../database/connection'
+import { useDB } from '../database/connection'
 import userValidation from '../validations/user.validation'
 import { SendLoginResponse } from '../utils/apiResponse'
-import { adminSignIn, loginStaffUser, studentSignIn } from './controller/signIn.controller'
+import { adminSignIn, userSignIn } from './controller/signIn.controller'
 import { env } from '../utils/env'
-import userSchema, { type IUser } from '../schema/institute/user'
-import studentSchema, { createModel as createStudentModel } from '../schema/institute/student'
 import AppError from '../utils/appError'
 import httpStatus from 'http-status'
+
+// models
+import { createModel as createStaffModel } from '../schema/institute/user'
+import { createModel as createStudentModel } from '../schema/institute/student'
+import { createModel as createAdminModel } from '../schema/master/admin'
+import { USER_TYPES } from '../data/constants'
 
 type AuthRequestHandler = (
   req: AuthenticatedRequest,
@@ -16,7 +20,7 @@ type AuthRequestHandler = (
   next: NextFunction
 ) => Promise<void>
 
-interface LoginResponse extends IUser {
+interface LoginResponse {
   token: string
 }
 
@@ -26,45 +30,44 @@ const cookieOptions = {
   path: '/'
 }
 
-const loginTypes = {
-  staff: studentSignIn,
-  admin: adminSignIn,
-  student: studentSignIn
-}
-
 export const loginAccount: AuthRequestHandler = async (req, res, next) => {
   // const ipAddress = req.headers['x-forwarded-for'] ?? req.connection.remoteAddress
   try {
     const loginData: LoginData = await userValidation.login.validateAsync(req.body)
+    console.log(req.body.userType)
+    const { userType, institutionName, remember } = loginData
 
-    // 1. validate userType
-    const isValidUserType = Object.keys(loginTypes).includes(loginData.userType)
-    if (!isValidUserType) {
-      throw new AppError(httpStatus.BAD_REQUEST, `'${loginData.userType}' is not a valid userType.`)
-    }
-
-    const institutionDB = await useDB(loginData.institutionName)
+    let user: LoginResponse = { token: '' }
     let model
 
-    switch (loginData.userType) {
-      case 'student':
-        model = createStudentModel(institutionDB)
-        break
-      case 'staff':
-        model = createModel(institutionDB, 'user', userSchema)
-        break
+    // 1. validate userType
+    // if (Object.values(USER_TYPES).includes(userType)) {
+    //   throw new AppError(httpStatus.BAD_REQUEST, `'${userType}' is not a valid userType.`)
+    // }
 
-      default:
-        throw new AppError(
-          httpStatus.BAD_REQUEST,
-          `'${loginData.userType}' is not a valid userType.`
-        )
+    if (userType === USER_TYPES.ADMIN) {
+      const masterDB = await useDB('master_database')
+      model = createAdminModel(masterDB)
+      // 1. Login admin user
+      user = await adminSignIn(loginData, model)
+    } else {
+      const institutionDB = await useDB(institutionName)
+
+      switch (userType) {
+        case USER_TYPES.STUDENT:
+          model = createStudentModel(institutionDB)
+          break
+        case USER_TYPES.STAFF:
+          model = createStaffModel(institutionDB)
+          break
+        default:
+          throw new AppError(httpStatus.BAD_REQUEST, `'${userType}' is not a valid userType.`)
+      }
+
+      user = await userSignIn(loginData, model)
     }
 
-    const loginByUserType = loginTypes[loginData.userType]
-    const user: LoginResponse = await loginByUserType(loginData, model)
-
-    const { remember } = loginData
+    // Generate token & send Response
     const isLocalhost = req.hostname === 'localhost'
 
     const ONE_DAY = 24 * 60 * 60 * 1000 // Cookie expires after 1 day
