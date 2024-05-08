b630bfd9c63966b6b92decf2c30da5c3427d32e3
Raghvendra Yadav <raghvendraa.dev@gmail.com>
2024-05-09 01:10:52 +0530

    fix

--- index.ts
+++ index.ts
@@ -8,13 +8,13 @@ moduleAlias.addAliases({
 /* eslint-disable import/first */
 import 'dotenv/config'
 import app from './src/app'
-import io from '@/config/socket-io'
+// import io from '@/config/socket-io'
 import appConstants from '@/config/appConstants'
 
-const { API_PORT, SOCKET_IO_PORT } = appConstants
+const { API_PORT } = appConstants
 
 // socket io
-io.listen(SOCKET_IO_PORT)
+// io.listen(SOCKET_IO_PORT)
 
 app.listen(API_PORT, () => {
   console.log(`________________server is running at port ${API_PORT}.`)
